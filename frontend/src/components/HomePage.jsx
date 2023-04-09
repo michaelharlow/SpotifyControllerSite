a9bd6e986b4eb032e67c6611a86e4ad3e06d6d17
Michael Harlow <michaelharlow1@outlook.com>
2023-04-09 14:01:00 -0500

    ability to leave room

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
-import { BrowserRouter, Routes, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import Room from "./Room";
 
 const Home = () => {
@@ -28,10 +28,24 @@ const Home = () => {
 };
 
 export default function HomePage() {
+  const [roomCode, setRoomCode] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        setRoomCode(data.code);
+      });
+  });
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route
+          exact
+          path="/"
+          element={roomCode ? <Navigate to={`/room/${roomCode}`} /> : <Home />}
+        />
         <Route path="/join" element={<RoomJoinPage />} />
         <Route path="/create" element={<CreateRoomPage />} />
         <Route path="/room/:roomCode" element={<Room />} />
--- frontend/src/components/Room.jsx
+++ frontend/src/components/Room.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Grid, Button, Typography } from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function Room() {
   let defaultVotes = 2;
+  let navigate = useNavigate();
   let { roomCode } = useParams();
 
   const [guestCanPause, setGuestCanPause] = useState("");
@@ -11,20 +13,66 @@ export default function Room() {
 
   useEffect(() => {
     fetch(`/api/get-room?code=${roomCode}`)
-      .then((Response) => Response.json())
+      .then((response) => {
+        if (!response.ok) {
+          navigate("/");
+          //throws error to prevent bad data read
+          throw new Error("Bad room code");
+        }
+        response.json();
+      })
       .then((data) => {
         setVotesToSkip(data.votes_to_skip);
         setGuestCanPause(data.guest_can_pause);
         setIsHost(data.is_host);
+      })
+      .catch((error) => {
+        console.log(error);
       });
   }, []);
 
+  const onLeaveButtonPress = () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+
+    fetch("/api/leave-room", requestOptions).then((_response) => {
+      navigate("/");
+    });
+  };
+
   return (
-    <div>
-      <h1>{roomCode}</h1>
-      <p>Votes: {votesToSkip}</p>
-      <p>Guest Can Pause: {guestCanPause.toString()}</p>
-      <p>Is host: {isHost.toString()}</p>
-    </div>
+    <Grid container spacing={1} alignItems="center" direction="column">
+      <Grid item xs={12}>
+        <Typography variant="h4" component="h4">
+          Code: {roomCode}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6" component="h6">
+          Votes: {votesToSkip}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6" component="h6">
+          Guest Can Pause: {guestCanPause.toString()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6" component="h6">
+          Is host: {isHost.toString()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={onLeaveButtonPress}
+        >
+          Leave Room
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
