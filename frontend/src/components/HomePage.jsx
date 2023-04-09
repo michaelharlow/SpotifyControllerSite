45e20dd1ae16c9bf7632a329b49af8c578d86fb1
Michael Harlow <michaelharlow1@outlook.com>
2023-04-09 11:35:27 -0500

    home page revamp

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
 import { BrowserRouter, Routes, Route, Link, Redirect } from "react-router-dom";
 import Room from "./Room";
+
+const Home = () => {
+  return (
+    <Grid container spacing={3} alignItems="center" direction="column">
+      <Grid item xs={12}>
+        <Typography variant="h3" compact="h3">
+          Spotify Party
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <ButtonGroup disableElevation variant="contained" color="primary">
+          <Button color="primary" to="/join" component={Link}>
+            Join a Room
+          </Button>
+          <Button color="secondary" to="/create" component={Link}>
+            Create a Room
+          </Button>
+        </ButtonGroup>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default function HomePage() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<p>this is the home page!!</p>} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/join" element={<RoomJoinPage />} />
         <Route path="/create" element={<CreateRoomPage />} />
         <Route path="/room/:roomCode" element={<Room />} />
